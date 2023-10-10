5131c72d54500ecc11f5e33e4c053d83eb039277
Andrea Sansone <andreasansone35@gmail.com>
2023-10-10 12:15:45 +0200

    working on the form

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,22 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screen: {
+        Fm: "376px",
+      },
+      fontFamily: {
+        sans: ["Space Mono", "sans-serif"],
+      },
+      colors: {
+        "Fm-Strong-cyan": "hsl(172, 67%, 45%)",
+        "Fm-Very-dark-cyan": "hsl(183, 100%, 15%)",
+        "Fm-Dark-grayish-cyan": "hsl(186, 14%, 43%)",
+        "Fm-Grayish-cyan": "hsl(184, 14%, 56%)",
+        "Fm-Light-grayish-cyan": "hsl(185, 41%, 84%)",
+        "Fm-Very-light-grayish-cyan": "hsl(189, 41%, 97%)",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
